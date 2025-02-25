35394afa2f90abc8fd046aedb298e7d018afc69d
Matthew White <a02366266@aggies.usu.edu>
2025-02-25 14:24:17 -0700

    Downloaded 1981 to 1990 PRISM data

--- Scripts/download_prism_sap_seasons.R
+++ Scripts/download_prism_sap_seasons.R
@@ -3,14 +3,14 @@ download_prism(sp_res = "4km", data = c("tmin", "tmax"),
                start_date = as.Date("1991-01-01"),
                end_date = as.Date("1991-04-30"),
                t_res = "daily",
-               out_dir = "PRISM_Sap_Seasons_Data/1991")
+               out_dir = "Data_Raw/PRISM_Sap_Seasons_Data/1991")
 
 # 2020 test
 download_prism(sp_res = "4km", data = c("tmin", "tmax"),
                start_date = as.Date("2020-01-01"),
                end_date = as.Date("2020-04-30"),
                t_res = "daily",
-               out_dir = "PRISM_Sap_Seasons_Data/2020")
+               out_dir = "Data_Raw/PRISM_Sap_Seasons_Data/2020")
 
 # Download the rest of the years
 for (year in 1992:2019) {
@@ -20,5 +20,16 @@ for (year in 1992:2019) {
                  start_date = my_start_date,
                  end_date = my_end_date,
                  t_res = "daily",
-                 out_dir = paste0("PRISM_Sap_Seasons_Data/", year))
+                 out_dir = paste0("Data_Raw/PRISM_Sap_Seasons_Data/", year))
+}
+
+# Download missing 1981 to 1990 data
+for (year in 1981:1990) {
+  my_start_date <- as.Date(paste0(year, "-01-01"))
+  my_end_date <- as.Date(paste0(year, "-04-30"))
+  download_prism(sp_res = "4km", data = c("tmin", "tmax"),
+                 start_date = my_start_date,
+                 end_date = my_end_date,
+                 t_res = "daily",
+                 out_dir = paste0("Data_Raw/PRISM_Sap_Seasons_Data/", year))
 }
