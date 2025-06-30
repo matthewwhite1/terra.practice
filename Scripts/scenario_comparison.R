901a0599388feb62495a445ac90fa3470af2695c
Matthew White <a02366266@aggies.usu.edu>
2025-06-30 16:55:36 -0600

    Wrote script for calculating within scenario mean and variance and between scenario mean and variance

--- Scripts/scenario_comparison.R
+++ Scripts/scenario_comparison.R
@@ -1,1 +1,115 @@
 ### Within scenario ssp585, first run of first three LOCA2 models
+# Define stuff
+models <- c("ACCESS-CM2", "ACCESS-ESM1-5", "AWI-CM-1-1-MR")
+ssp585_sap <- vector("list", 3)
+k_upper <- 2.2 + 273.15
+k_lower <- -1.1 + 273.15
+
+## Create sap days for all models
+for (i in 1:3) {
+  # Load in rasters
+  path <- paste0("D:/Data/LOCA2/", models[i], "/0p0625deg/r1i1p1f1")
+  loca_rast <- loca_t_rast(path)
+
+  # Calculate sap days for model
+  model_sap_day <- sap_day(loca_rast$tmax, loca_rast$tmin, k_upper, k_lower)
+  ssp585_sap[[i]] <- model_sap_day
+
+  # Write rasters to drive
+  propname <- paste0("D:/Data/LOCA2/", models[i], "_run1_ssp585_prop.tif")
+  terra::writeRaster(model_sap_day$proportion, propname)
+  sumname <- paste0("D:/Data/LOCA2/", models[i], "_run1_ssp585_sum.tif")
+  terra::writeRaster(model_sap_day$sum, sumname)
+}
+
+## Calculate within scenario mean and variance
+# Define stuff
+years <- as.integer(terra::names(ssp585_sap[[1]]$proportion))
+prop_mean_list <- vector("list", length(years))
+prop_var_list <- vector("list", length(years))
+
+# For each year
+for (i in seq_along(years)) {
+  # Combine into one raster
+  combined <- c(ssp585_sap[[1]]$proportion[[i]],
+                ssp585_sap[[2]]$proportion[[i]],
+                ssp585_sap[[3]]$proportion[[i]])
+
+  # Calculate mean and variance
+  prop_mean_list[[i]] <- terra::app(combined, mean)
+  prop_var_list[[i]] <- terra::app(combined, var)
+
+  # Print progress
+  print(paste0("Completed calculation for year ", years[i]))
+}
+
+# Combine raster stacks and write to drive
+ssp585_prop_mean <- terra::rast(prop_mean_list)
+names(ssp585_prop_mean) <- years
+terra::writeRaster(ssp585_prop_mean, "D:/Data/LOCA2/ssp585_prop_mean.tif")
+
+# Combine raster stacks and write to drive
+ssp585_prop_var <- terra::rast(prop_var_list)
+names(ssp585_prop_var) <- years
+terra::writeRaster(ssp585_prop_var, "D:/Data/LOCA2/ssp585_prop_var.tif")
+
+
+
+
+### Between scenarios for "ACCESS-CM2"
+# Define stuff
+scenarios <- c("ssp245", "ssp370")
+accesscm2_sap <- vector("list", 3)
+k_upper <- 2.2 + 273.15
+k_lower <- -1.1 + 273.15
+
+## Create sap days for all models
+for (i in 1:2) {
+  # Load in rasters
+  path <- "D:/Data/LOCA2/ACCESS-CM2/0p0625deg/r1i1p1f1"
+  loca_rast <- loca_t_rast(path, c("historical", scenarios[i]))
+
+  # Calculate sap days for model
+  model_sap_day <- sap_day(loca_rast$tmax, loca_rast$tmin, k_upper, k_lower)
+  accesscm2_sap[[i]] <- model_sap_day
+
+  # Write rasters to drive
+  propname <- paste0("D:/Data/LOCA2/ACCESS-CM2_run1_", scenarios[i], "_prop.tif")
+  terra::writeRaster(model_sap_day$proportion, propname)
+  sumname <- paste0("D:/Data/LOCA2/ACCESS-CM2_run1_", scenarios[i], "_sum.tif")
+  terra::writeRaster(model_sap_day$sum, sumname)
+}
+
+# Third entry in list has already been calculated
+accesscm2_sap[[3]] <- ssp585_sap[[1]]
+
+## Calculate within scenario mean and variance
+# Define stuff
+years <- as.integer(terra::names(accesscm2_sap[[1]]$proportion))
+prop_mean_list <- vector("list", length(years))
+prop_var_list <- vector("list", length(years))
+
+# For each year
+for (i in seq_along(years)) {
+  # Combine into one raster
+  combined <- c(accesscm2_sap[[1]]$proportion[[i]],
+                accesscm2_sap[[2]]$proportion[[i]],
+                accesscm2_sap[[3]]$proportion[[i]])
+
+  # Calculate mean and variance
+  prop_mean_list[[i]] <- terra::app(combined, mean)
+  prop_var_list[[i]] <- terra::app(combined, var)
+
+  # Print progress
+  print(paste0("Completed calculation for year ", years[i]))
+}
+
+# Combine raster stacks and write to drive
+accesscm2_sap_prop_mean <- terra::rast(prop_mean_list)
+names(accesscm2_sap_prop_mean) <- years
+terra::writeRaster(accesscm2_sap_prop_mean, "D:/Data/LOCA2/allscenarios_prop_mean.tif")
+
+# Combine raster stacks and write to drive
+accesscm2_sap_prop_var <- terra::rast(prop_var_list)
+names(accesscm2_sap_prop_var) <- years
+terra::writeRaster(accesscm2_sap_prop_var, "D:/Data/LOCA2/allscenarios_prop_var.tif")
